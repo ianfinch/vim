7b519359c869b09a7c543c489708c6bf79b2085d
Ian Finch <ianfinch@users.noreply.github.com>
2026-02-19 15:54:06 +0000

    Added a couple of git keymaps

--- lua/custom/git.lua
+++ lua/custom/git.lua
@@ -20,9 +20,10 @@ require("gitsigns").setup({
 
 -- For git signs
 vim.keymap.set("", "<Leader>gd", ":Gitsigns preview_hunk<CR>", { desc = "Git diff current hunk" })
-vim.keymap.set("", "<Leader>gD", ":Gitsigns preview_hunk_inline<CR>", { desc = "Git diff current hunk inline" })
 vim.keymap.set("", "<Leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle inline git blame" })
 
 -- For fugitive
 vim.keymap.set("", "<Leader>gB", ":Git blame<CR>", { desc = "Git blame" })
-vim.keymap.set("", "<Leader>gl", ":Git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit<CR>", { silent = true, desc = "Git log" })
+vim.keymap.set("", "<Leader>gD", ":Gvdiffsplit<CR>", { desc = "Git diff split window" })
+vim.keymap.set("", "<Leader>gg", ":Git<CR>", { desc = "Open git" })
+vim.keymap.set("", "<Leader>gl", ":Gllog<CR>", { silent = true, desc = "Git log" })
